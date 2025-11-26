57f5689d788b70e0107c6268bc295fba383d67b5
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-11-26 21:03:14 +0600

    Do not compelete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,14 @@
-
 import "./globals.css";
 import { Inter, Roboto, Poppins } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-import { ThemeProvider } from "@/context/ThemeContext";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
+import { ThemeProvider } from "../../context/ThemeContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Lama Dev",
+  title: "StoryNest",
   description: "This is the description",
 };
 
