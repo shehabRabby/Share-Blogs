63980dfebf04caec39a0ec6ff26c2284ce67ff20
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-11-26 16:13:52 +0600

    Show hero section in home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "../../public/hero.png";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
   return (
-    <div>hello</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+
+        <Button url="/portfolio" text="See Our Works"></Button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
+    </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>©2023 Lamamia. All rights reserved.</div>
+      <div>©2025 StoryNest. All rights reserved.</div>
       <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Lama Dev Facebook Account" />
-        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image src="/1.png" width={25} height={25} className={styles.icon} alt="Shere nest" />
+        <Image src="/2.png" width={25} height={25} className={styles.icon} alt="Shere nest" />
+        <Image src="/3.png" width={25} height={25} className={styles.icon} alt="Shere nest" />
+        <Image src="/4.png" width={25} height={25} className={styles.icon} alt="Shere nest" />
       </div>
     </div>
   );
